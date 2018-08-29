1f60439a42ff10fb66527c722fd7ca9747fa4809
B1nary Drag0n <marielynemathon@gmail.com>
2018-08-28 21:06:25 -0400

    Added new blinking options.

--- LEDMadness.py
+++ LEDMadness.py
@@ -6,8 +6,16 @@ ledPin = 11 # RPI GPIO17 pin
 
 ledOn = "o"
 ledOff = "i"
-ledBlink = "b"
+ledBlinkLowSpeed = "1"
+ledBlinkMediumSpeed = "2"
+ledBlinkHighSpeed = "3"
+ledBlinkUltraSpeed = "4"
 exitApp = "e"
+
+lowSpeed = 1
+mediumSpeed = 0.5
+highSpeed = 0.2
+ultraSpeed = 0.05
 
 ledIsOn = False
 
@@ -19,19 +27,29 @@ def setup():
 
 def turn_on_LED():
 	GPIO.output(ledPin, GPIO.HIGH)
+	global ledIsOn
+	ledIsOn = True
+	print("LED ON")
 
 def turn_off_LED():
 	GPIO.output(ledPin, GPIO.LOW)
+	global ledIsOn
+	ledIsOn = False
+	print("LED OFF")
 
-def blink_LED():
+def blink_LED(speed):
 	print("!!! BLINK BLINK !!!")
 	x = 0
-	while x < 25:
+	while x < 10:
 		GPIO.output(ledPin, GPIO.HIGH)
-		time.sleep(0.05)
+		time.sleep(speed)
 		GPIO.output(ledPin, GPIO.LOW)
-		time.sleep(0.05)
+		time.sleep(speed)
 		x += 1
+	if ledIsOn:
+		GPIO.output(ledPin, GPIO.HIGH)
+	else:
+		GPIO.output(ledPin, GPIO.LOW)
 
 def loop():
 	while True:
@@ -40,8 +58,14 @@ def loop():
 			turn_on_LED()
 		elif pressedKey == ledOff:
 			turn_off_LED()
-		elif pressedKey == ledBlink:
-			blink_LED()
+		elif pressedKey == ledBlinkLowSpeed:
+			blink_LED(lowSpeed)
+		elif pressedKey == ledBlinkMediumSpeed:
+			blink_LED(mediumSpeed)
+		elif pressedKey == ledBlinkHighSpeed:
+			blink_LED(highSpeed)
+		elif pressedKey == ledBlinkUltraSpeed:
+			blink_LED(ultraSpeed)
 		elif pressedKey == exitApp:
 			destroy()
 			break
